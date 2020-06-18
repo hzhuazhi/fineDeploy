4068ca5721a0a392b49925dc68d93588e4a0e923
tangzhulong12 <345061813@qq.com>
2020-06-18 17:01:58 +0800

    添加小薇审核机制

---
+++ src/com/xn/pay/dao/CatDataBindingDao.java
@@ -0,0 +1,11 @@
+package com.xn.pay.dao;
+
+import com.xn.common.dao.BaseDao;
+
+/**
+ * @Description TODO
+ * @Date 2020/6/16 21:11
+ * @Version 1.0
+ */
+public interface CatDataBindingDao  <T> extends BaseDao<T> {
+}
